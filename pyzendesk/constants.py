afcb907602f00828ca09c3b0ce55ce359ed19727
Muflone <webreg@muflone.com>
2021-12-08 10:02:58 +0100

    Added pyzendesk package

---
+++ pyzendesk/constants.py
@@ -0,0 +1,28 @@
+##
+#     Project: PyZendesk
+# Description: API for Zendesk
+#      Author: Fabio Castelli (Muflone) <muflone@muflone.com>
+#   Copyright: 2021 Fabio Castelli
+#     License: GPL-3+
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+##
+
+APP_NAME = 'PyZendesk'
+APP_VERSION = '0.1.0'
+APP_DESCRIPTION = 'API for Zendesk'
+APP_ID = 'pyzendesk.muflone.com'
+APP_URL = 'http://www.muflone.com/pyzendesk/'
+APP_AUTHOR = 'Fabio Castelli'
+APP_AUTHOR_EMAIL = 'muflone@muflone.com'
+APP_COPYRIGHT = 'Copyright 2021 %s' % APP_AUTHOR
